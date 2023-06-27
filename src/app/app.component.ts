afc2de31c417ffd41916ed87b1467a85b3a1511a
Leonardo Costa <leonardo.costa@unifesp.br>
2023-06-26 21:32:06 -0300

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'wia';
+export class AppComponent implements OnInit {
+  constructor(private meta: Meta, private title: Title) {}
+
+  ngOnInit() {
+    this.meta.updateTag({
+      name: 'viewport',
+      content:
+        'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+    });
+    this.title.setTitle('WIA');
+  }
 }
